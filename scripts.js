28bca13fadae4a19bb0e47fe2a12775af3dbe5f7
TQIT02 <taylor1011.disalvo@gmail.com>
2020-09-30 23:36:06 -0400

    Finishing the website redesign, now with JQuery

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+  $("div.food").hide();
+  $("div.gaming").hide();
+  $("div.music").hide();
 
-    // jQuery methods go here...
-  
+  $("button#food").click(function(){
+    $("div.food").toggle(100);
   });
+
+  $("button#gaming").click(function(){
+    $("div.gaming").toggle(100);
+  });
+
+  $("button#music").click(function(){
+    $("div.music").toggle(100);
+  });
+
+  $(".my_name").hover(function(){
+    $("a.easter_egg").css("color", "black");
+  });
+
+});
